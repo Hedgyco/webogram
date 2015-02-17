9530a465b5e9ad5b4bb43b56ae04c92d5b3d67f7
Juan Pineda <juan@logician.com>
2015-02-16 16:15:41 -0800

    Force heroku rebuild.

--- server.js
+++ server.js
@@ -96,6 +96,13 @@ StaticServlet.MimeMap = {
 
 StaticServlet.prototype.handleRequest = function(req, res) {
   var self = this;
+  console.log("foo", req.url.href)
+  if (req.headers['x-forwarded-proto']!='https') {
+//  if (req.connection.encrypted) {
+    console.log("redirect", req.headers['x-forwarded-proto'], req.url);
+    //return self.sendForbidden_(req, res, "foo");
+    return self.sendRedirect_(req, res, 'http://' + req.url.host + '/' + req.url.path)
+  }
   if (req.url.pathname == '/') {
     req.url.pathname = "/index.html"
   }
